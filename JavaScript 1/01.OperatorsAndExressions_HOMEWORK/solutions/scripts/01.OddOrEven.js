5e40eba6ef8faf0c613e4b2e943fdd030aa44a9a
theexar <easlavov@gmail.com>
2014-04-29 12:50:53 +0300

    JS1 Homework Rearranged

---
+++ JavaScript 1/01.OperatorsAndExressions_HOMEWORK/solutions/scripts/01.OddOrEven.js
@@ -0,0 +1,16 @@
+﻿function printOddEven(integer) {
+    if (integer % 2 === 0) {
+        jsConsole.writeLine("The integer is even.");
+    } else {
+        jsConsole.writeLine("The integer is odd.");
+    }
+}
+
+jsConsole.writeLine("Testing the script with two integers.");
+jsConsole.writeLine("The first integer is 134: ");
+printOddEven(134);
+jsConsole.writeLine("The second integer is 13: ");
+printOddEven(13);
+
+jsConsole.writeLine("");
+jsConsole.writeLine("Test complete");
---
+++ JavaScript 1/01.OperatorsAndExressions_HOMEWORK/solutions/scripts/02.CheckIfDivisibleBy5and7.js
@@ -0,0 +1,7 @@
+﻿var inputInteger = 105;
+if (inputInteger % 35 === 0) {
+    jsConsole.writeLine("The integer is divisible by 5 and 7.")
+}
+else {
+    jsConsole.writeLine("The integer is not divisible by 5 and 7.")
+}
