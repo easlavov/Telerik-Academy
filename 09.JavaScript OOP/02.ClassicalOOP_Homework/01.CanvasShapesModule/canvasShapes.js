4b7e33ba53094aaff20e7978fdade1eb73ac1143
theexar <easlavov@gmail.com>
2014-06-27 11:24:56 +0300

    canvas shapes module refactoring

--- 09.JavaScript OOP/02.ClassicalOOP_Homework/01.CanvasShapesModule/canvasShapes.js
+++ 09.JavaScript OOP/02.ClassicalOOP_Homework/01.CanvasShapesModule/canvasShapes.js
@@ -1,46 +1,8 @@
 var CanvasShapes = function(canvasId){
-    var context = getContext(canvasId);
-    var DEFAULT_FILL_COLOR = 'black';
-    var DEFAULT_STROKE_COLOR = 'black';
-    var DEFAULT_STROKE_WIDTH = 2;
-
-    function fillRectangle(x, y, width, height, fillColor) {
-        context.fillStyle = fillColor || DEFAULT_FILL_COLOR;
-        context.fillRect(x, y, width, height);
-    }
-
-    function strokeRectangle(x, y, width, height, strokeColor, strokeWidth) {
-        context.strokeStyle = strokeColor || DEFAULT_STROKE_COLOR;
-        context.lineWidth = strokeWidth || DEFAULT_STROKE_WIDTH;
-        context.strokeRect(x, y, width, height);
-    }
-
-    function fillCircle(x, y, radius, fillColor) {
-        context.fillStyle = fillColor || DEFAULT_FILL_COLOR;
-        context.beginPath();
-        context.arc(x, y, radius, 0, 2 * Math.PI, true);
-        context.fill();
-        context.closePath();
-    }
-
-    function strokeCircle(x, y, radius, strokeColor, strokeWidth) {
-        context.strokeStyle = strokeColor || DEFAULT_STROKE_COLOR;
-        context.lineWidth = strokeWidth || DEFAULT_STROKE_WIDTH;
-        context.beginPath();
-        context.arc(x, y, radius, 0, 2 * Math.PI, true);
-        context.stroke();
-        context.closePath();
-    }
-
-    function line(x1, y1, x2, y2, color, strokeWidth) {
-        context.strokeStyle = color || DEFAULT_STROKE_COLOR;
-        context.lineWidth = strokeWidth || DEFAULT_STROKE_WIDTH;
-        context.beginPath();
-        context.moveTo(x1, y1);
-        context.lineTo(x2, y2);
-        context.stroke();
-        context.closePath();
-    }
+    this.context = getContext(canvasId);
+    this.DEFAULT_FILL_COLOR = 'black';
+    this.DEFAULT_STROKE_COLOR = 'black';
+    this.DEFAULT_STROKE_WIDTH = 2;
 
     function getContext(id) {
         var canvas = document.getElementById(id);
@@ -51,12 +13,44 @@ var CanvasShapes = function(canvasId){
         var ctx = canvas.getContext('2d');
         return ctx;
     }
+};
 
-    return {
-        fillRectangle : fillRectangle,
-        strokeRectangle : strokeRectangle,
-        fillCircle : fillCircle,
-        strokeCircle : strokeCircle,
-        line : line
+CanvasShapes.prototype = {
+    fillRectangle : function fillRectangle(x, y, width, height, fillColor) {
+        this.context.fillStyle = fillColor || this.DEFAULT_FILL_COLOR;
+        this.context.fillRect(x, y, width, height);
+    },
+
+    strokeRectangle : function strokeRectangle(x, y, width, height, strokeColor, strokeWidth) {
+        this.context.strokeStyle = strokeColor || this.DEFAULT_STROKE_COLOR;
+        this.context.lineWidth = strokeWidth || this.DEFAULT_STROKE_WIDTH;
+        this.context.strokeRect(x, y, width, height);
+    },
+
+    fillCircle : function fillCircle(x, y, radius, fillColor) {
+        this.context.fillStyle = fillColor || this.DEFAULT_FILL_COLOR;
+        this.context.beginPath();
+        this.context.arc(x, y, radius, 0, 2 * Math.PI, true);
+        this.context.fill();
+        this.context.closePath();
+    },
+
+    strokeCircle : function strokeCircle(x, y, radius, strokeColor, strokeWidth) {
+        this.context.strokeStyle = strokeColor || this.DEFAULT_STROKE_COLOR;
+        this.context.lineWidth = strokeWidth || this.DEFAULT_STROKE_WIDTH;
+        this.context.beginPath();
+        this.context.arc(x, y, radius, 0, 2 * Math.PI, true);
+        this.context.stroke();
+        this.context.closePath();
+    },
+
+    line : function line(x1, y1, x2, y2, color, strokeWidth) {
+        this.context.strokeStyle = color || this.DEFAULT_STROKE_COLOR;
+        this.context.lineWidth = strokeWidth || this.DEFAULT_STROKE_WIDTH;
+        this.context.beginPath();
+        this.context.moveTo(x1, y1);
+        this.context.lineTo(x2, y2);
+        this.context.stroke();
+        this.context.closePath();
     }
 };
