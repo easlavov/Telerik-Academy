ce28d94c91edd10678d7c65f836977f37d1f6a31
theexar <easlavov@gmail.com>
2014-07-07 22:28:41 +0300

    typescript hw first upload

---
+++ 09.JavaScript OOP/TypeScript_HOMEWORK/01.Hierarchy/app.js
@@ -0,0 +1,100 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var Persons;
+(function (Persons) {
+    var Degree = (function () {
+        function Degree(discipline, degree) {
+            this.discipline = discipline;
+            this.degree = degree;
+            if (degree < 0) {
+                throw {
+                    message: "Degree can't be negative."
+                };
+            }
+
+            this.discipline = discipline;
+            this.degree = degree;
+        }
+        return Degree;
+    })();
+    Persons.Degree = Degree;
+
+    var Person = (function () {
+        function Person(name, age) {
+            this.name = name;
+            this.age = age;
+            this.name = name;
+            this.age = age;
+        }
+        Person.prototype.greet = function () {
+            var greeting = 'Helo, my name is ' + this.name;
+            return greeting;
+        };
+        return Person;
+    })();
+    Persons.Person = Person;
+
+    var Student = (function (_super) {
+        __extends(Student, _super);
+        function Student(name, age, grade) {
+            _super.call(this, name, age);
+            this.name = name;
+            this.age = age;
+            this.grade = grade;
+            this.grade = grade;
+            this.degrees = [];
+        }
+        Student.prototype.addDegree = function (discipline, degree) {
+            var newDegree = new Degree(discipline, degree);
+            this.degrees.push(newDegree);
+        };
+        return Student;
+    })(Person);
+    Persons.Student = Student;
+
+    var Teacher = (function (_super) {
+        __extends(Teacher, _super);
+        function Teacher(name, age, specialty) {
+            _super.call(this, name, age);
+            this.name = name;
+            this.age = age;
+            this.specialty = specialty;
+            this.specialty = specialty;
+        }
+        return Teacher;
+    })(Person);
+    Persons.Teacher = Teacher;
+})(Persons || (Persons = {}));
+
+var School;
+(function (School) {
+    var Class = (function () {
+        function Class() {
+            this.students = [];
+        }
+        Class.prototype.addStudent = function (student) {
+            this.students.push(student);
+        };
+
+        Class.prototype.setTeacher = function (teacher) {
+            this.teacher = teacher;
+        };
+
+        Class.prototype.getLowestGradeInClassInDiscipline = function (discipline) {
+            var lowestGrade = 0;
+            this.students.forEach(function (student) {
+            });
+        };
+        return Class;
+    })();
+    School.Class = Class;
+})(School || (School = {}));
+
+var testPerson = new Persons.Person('Emo', 25);
+debugger;
+console.log(testPerson.greet());
+//# sourceMappingURL=app.js.map
---
+++ 09.JavaScript OOP/TypeScript_HOMEWORK/01.Hierarchy/app.ts
@@ -0,0 +1,96 @@
+﻿module Interfaces {
+    export interface IPerson {
+        name: string;
+        age: number;
+        greet();
+    }
+
+    export interface IStudent extends IPerson {
+        grade: number;
+        degrees: Persons.Degree[];
+    }
+
+    export interface ITeacher extends IPerson {
+        specialty: string;
+    }
+}
+
+module Persons {
+    export class Degree {
+        constructor(public discipline: string, public degree: number) {
+            if (degree < 0) {
+                throw {
+                    message: "Degree can't be negative."
+                }
+            }
+
+            this.discipline = discipline;
+            this.degree = degree;
+        }
+    }
+
+    export class Person implements Interfaces.IPerson {
+        constructor(public name: string, public age: number) {
+            this.name = name;
+            this.age = age;
+        }
+
+        greet() {
+            var greeting = 'Helo, my name is ' + this.name;
+            return greeting;
+        }
+    }
+
+    export class Student extends Person implements Interfaces.IStudent {
+        degrees: Degree[];
+
+        constructor(public name: string, public age: number, public grade: number) {
+            super(name, age);
+            this.grade = grade;
+            this.degrees = [];
+        }
+
+        addDegree(discipline: string, degree: number) {
+            var newDegree = new Degree(discipline, degree);
+            this.degrees.push(newDegree);
+        }
+    }
+
+    export class Teacher extends Person implements Interfaces.ITeacher {
+        constructor(public name: string, public age: number, public specialty: string) {
+            super(name, age);
+            this.specialty = specialty;
+        }
+    }
+}
+
+module School {
+    export class Class {
+        students: Persons.Student[];
+        teacher: Persons.Teacher;
+
+        constructor() {
+            this.students = [];
+        }
+
+        addStudent(student: Persons.Student) {
+            this.students.push(student);
+        }
+
+        setTeacher(teacher: Persons.Teacher) {
+            this.teacher = teacher;
+        }
+
+        getLowestGradeInClassInDiscipline(discipline: string) {
+            var lowestGrade = 0;
+            this.students.forEach(function (student) {
+
+            });
+        }
+
+    }
+}
+
+var testPerson = new Persons.Person('Emo', 25);
+debugger;
+console.log(testPerson.greet());
