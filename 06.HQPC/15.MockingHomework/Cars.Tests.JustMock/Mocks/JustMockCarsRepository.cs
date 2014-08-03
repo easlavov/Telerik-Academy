3d3592302928afd9f4d91e477de42a9b12d97ce1
theexar <easlavov@gmail.com>
2014-08-03 17:39:32 +0300

    Mocking HW partial solution

---
+++ 06.HQPC/15.MockingHomework/Cars.Tests.JustMock/Mocks/JustMockCarsRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace Cars.Tests.JustMock.Mocks
+{
+    using Cars.Contracts;
+    using Cars.Models;
+    using System.Linq;
+    using Telerik.JustMock;
+
+    public class JustMockCarsRepository : CarRepositoryMock, ICarsRepositoryMock
+    {
+        protected override void ArrangeCarsRepositoryMock()
+        {
+            this.CarsData = Mock.Create<ICarsRepository>();
+            Mock.Arrange(() => this.CarsData.Add(Arg.IsAny<Car>())).DoNothing();
+            Mock.Arrange(() => this.CarsData.All()).Returns(this.FakeCarCollection);
+            Mock.Arrange(() => this.CarsData.Search(Arg.AnyString)).Returns(this.FakeCarCollection.Where(c => c.Make == "BMW").ToList());
+            Mock.Arrange(() => this.CarsData.GetById(Arg.AnyInt)).Returns(this.FakeCarCollection.First());
+            // TotalCars getter mock
+            Mock.Arrange(() => this.CarsData.TotalCars).Returns(this.FakeCarCollection.Count);
+            // TODO: Remove mock
+            Mock.Arrange(() => this.CarsData.Remove(Arg.IsAny<Car>()))
+            // TODO: SortedByMake mock
+            // TODO: SortedByYear mock
+        }
+    }
+}
