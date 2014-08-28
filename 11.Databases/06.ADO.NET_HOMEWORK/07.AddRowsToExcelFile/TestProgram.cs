268dd3c15d4eabb0e07bb68e190f1f47f7635c7b
theexar <easlavov@gmail.com>
2014-08-28 16:56:57 +0300

    Task 7 solution

---
+++ 11.Databases/06.ADO.NET_HOMEWORK/07.AddRowsToExcelFile/TestProgram.cs
@@ -0,0 +1,28 @@
+﻿//Implement appending new rows to the Excel file.
+using System;
+using System.Data.OleDb;
+using System.Linq;
+
+class TestProgram
+{
+    static void Main()
+    {
+        string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source='..\\..\\scoreBoard.xls';Extended Properties=Excel 8.0;";
+        AddStudentScore(connectionString, "Tanas", "3.14");
+    }
+
+    static void AddStudentScore(string connectionString, string name, string score)
+    {
+        OleDbConnection dbCon = new OleDbConnection(connectionString);
+        dbCon.Open();
+        using (dbCon)
+        {
+            OleDbCommand xlCmd = new OleDbCommand(
+                "INSERT INTO [Sheet1$] (Name, Score) VALUES (@Name, @Score)", dbCon);
+            xlCmd.Parameters.AddWithValue("@Name", name);
+            xlCmd.Parameters.AddWithValue("@Score", score);
+            int affectedRows = xlCmd.ExecuteNonQuery();
+            Console.WriteLine("Affected rows: " + affectedRows);
+        }       
+    }
+}
