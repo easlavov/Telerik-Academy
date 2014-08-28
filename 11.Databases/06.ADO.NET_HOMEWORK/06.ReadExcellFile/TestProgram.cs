c0f9c6693abfdeb58a242fbb20dc0e5405bb8ec0
theexar <easlavov@gmail.com>
2014-08-28 16:36:39 +0300

    Task 6 solution

---
+++ 11.Databases/06.ADO.NET_HOMEWORK/06.ReadExcellFile/TestProgram.cs
@@ -0,0 +1,32 @@
+﻿//Create an Excel file with 2 columns: name and score: 
+//Write a program that reads your MS Excel file 
+//through the OLE DB data provider and displays the 
+//name and score row by row.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+class TestProgram
+{
+    static void Main()
+    {        
+
+        OleDbConnection dbCon = new OleDbConnection("provider=Microsoft.Jet.OLEDB.4.0;Data Source='..\\..\\scoreBoard.xls';Extended Properties=Excel 8.0;");
+        dbCon.Open();
+        using (dbCon)
+        {
+            OleDbCommand myCommand = new OleDbCommand("select * from [Sheet1$]", dbCon);            
+            OleDbDataReader reader = myCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                string name = (string)reader["Name"];
+                double score = (double)reader["Score"];
+                Console.WriteLine("{0} - {1}", name, score);
+            }
+        }        
+    }
+}
