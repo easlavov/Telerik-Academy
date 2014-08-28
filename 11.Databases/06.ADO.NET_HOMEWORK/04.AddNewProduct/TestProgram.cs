77a72d433333d96ba367676174ef5418bb9f25c1
theexar <easlavov@gmail.com>
2014-08-28 14:11:40 +0300

    Added solution to task 4

--- 11.Databases/06.ADO.NET_HOMEWORK/04.AddNewProduct/TestProgram.cs
+++ 11.Databases/06.ADO.NET_HOMEWORK/04.AddNewProduct/TestProgram.cs
@@ -1,13 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 class TestProgram
 {
     static void Main()
     {
+        string northwindConnectionString = "Server=.\\SQLEXPRESS; " +
+                                  "Database=Northwind; Integrated Security=true";
+        
+        AddNewProduct(northwindConnectionString, "Beer Astika");
+        AddNewProduct(northwindConnectionString, "Beer Ledenika");
+        AddNewProduct(northwindConnectionString, "Beer Ariana");
+    }
 
+    private static void AddNewProduct(string connectionString, string productName)
+    {
+        Console.WriteLine("Adding " + productName);
+        SqlConnection dbCon = new SqlConnection(connectionString);
+        dbCon.Open();
+        using (dbCon)
+        {
+            var addNewProdCmd = new SqlCommand(
+                "INSERT INTO Products(ProductName) VALUES(@prodName)",
+                dbCon);
+            addNewProdCmd.Parameters.AddWithValue("@prodName", productName);
+            var affectedRows = addNewProdCmd.ExecuteNonQuery();
+            Console.WriteLine("Affected rows: " + affectedRows);
+        }
     }
 }
