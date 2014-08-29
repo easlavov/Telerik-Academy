7cc691531dd3ed84bbe4edc64466bf1b62f994c3
theexar <easlavov@gmail.com>
2014-08-29 14:37:27 +0300

    HW rewritten. Added task 2 solution

---
+++ 11.Databases/07.EntityFramework_HOMEWORK/Northwind.DataAccessObject/DataAcessObject.cs
@@ -0,0 +1,70 @@
+﻿// Create a DAO class with static methods which 
+// provide functionality for inserting, modifying and 
+// deleting customers. Write a testing class.
+namespace Northwind.DataAccessObject
+{
+    using System;
+    using System.Linq;
+    using Northwind.Data;
+
+    public static class DataAcessObject
+    {
+        private const int CUSTOMER_ID_LENGTH = 5;
+
+        /// <summary>
+        /// Inserts a customer in the Northwind database.
+        /// </summary>
+        /// <param name="name">The name of the customer/</param>
+        public static void InsertCustomer(string name, string id)
+        {
+            ValidateCustomerIdLength(id);
+
+            var db = new NorthwindEntities();
+            var customer = new Customer();
+            customer.CompanyName = name;
+            customer.CustomerID = id;
+            db.Customers.Add(customer);
+            db.SaveChanges();
+        }
+
+        /// <summary>
+        /// Changes the company name of a selected customer in the Northwind database.
+        /// </summary>
+        /// <param name="customerId">The id of the customer.</param>
+        /// <param name="newName">The customers' new name.</param>
+        public static void ModifyCustomerName(string customerId, string newName)
+        {
+            ValidateCustomerIdLength(customerId);
+
+            var db = new NorthwindEntities();
+            var customer = db.Customers.First(x => x.CustomerID == customerId);
+            customer.CompanyName = newName;
+            db.SaveChanges();
+        }
+
+        /// <summary>
+        /// Deletes a customer from the Northwind database.
+        /// </summary>
+        /// <param name="customerId">The id of the customer.</param>
+        public static void DeleteCustomer(string customerId)
+        {
+            ValidateCustomerIdLength(customerId);
+
+            var db = new NorthwindEntities();
+            var customer = db.Customers.First(x => x.CustomerID == customerId);
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+        }
+
+        private static bool ValidateCustomerIdLength(string customerId)
+        {
+            if (customerId.Length != CUSTOMER_ID_LENGTH)
+            {
+                string message = string.Format("CustomerId must be {0} characters", CUSTOMER_ID_LENGTH);
+                throw new ArgumentOutOfRangeException(message);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 11.Databases/07.EntityFramework_HOMEWORK/Northwind.DataAccessObjectTestApplication/TestApplication.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.DataAccessObject;
+
+namespace Northwind.DataAccessObjectTestApplication
+{
+    class TestApplication
+    {
+        static void Main()
+        {
+            string welcomeMessage = "Hello! This app will guide you through the" +
+                                    " test I made to the DAO for the Nortwhind database." +
+                                    " Press a key to continue.";
+            Console.WriteLine(welcomeMessage);
+            Console.ReadKey();
+
+            AddTelerikAsCustomer();
+            ModifyTelerikName();
+            RemoveTelerikFromDatabase();
+
+            Console.WriteLine("Testing has finished.");
+        }
+
+        private static void AddTelerikAsCustomer()
+        {
+            string addCustomerMessage = "A customer with the name Telerik will be added to the" +
+                                           " Northwind database. Press a key to continue.";
+            Console.WriteLine(addCustomerMessage);
+            Console.ReadKey();
+            string name = "Telerik";
+            string id = "TELER";
+            DataAcessObject.InsertCustomer(name, id);
+        }
+
+        private static void ModifyTelerikName()
+        {
+            string modifyCustomerMessage = "The Telerik customers' name will be changed to Nakov" +
+                                           " in the Northwind database. Press a key to continue.";
+            Console.WriteLine(modifyCustomerMessage);
+            Console.ReadKey();
+            string name = "Nakov";
+            string id = "TELER";
+            DataAcessObject.ModifyCustomerName(id, name);
+        }
+
+        private static void RemoveTelerikFromDatabase()
+        {
+            string deleteCustomerMessage = "The Telerik customer will be deleted from" +
+                                           " the Northwind database. Press a key to continue.";
+            Console.WriteLine(deleteCustomerMessage);
+            Console.ReadKey();
+            string id = "TELER";
+            DataAcessObject.DeleteCustomer(id);
+        }
+    }
+}
