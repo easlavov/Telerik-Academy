ce7c76a76c10f67d7f7bfa47642b8c5762734dfe
theexar <easlavov@gmail.com>
2014-09-06 22:16:11 +0300

    MongoDB HW

---
+++ 11.Databases/11.MongoDB_HOMEWORK/ChatConsoleClient/Chat.cs
@@ -0,0 +1,47 @@
+﻿namespace ChatConsoleClient
+{
+    using MongoDB.Bson;
+    using MongoDB.Driver;
+    using System;
+    using System.Linq;
+
+    public class Chat
+    {
+        private MongoDatabase db;
+
+        public Chat(MongoDatabase database)
+        {
+            this.db = database;
+        }
+
+        public void Start()
+        {
+            var messages = this.db.GetCollection<BsonDocument>("Messages");
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+            while (true)
+            {
+                Console.Write("> ");
+                string input = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(input))
+                {
+                    var newMessage = new BsonDocument
+                    {
+                        { "Author", username },
+                        { "Text", input },
+                        { "Time", DateTime.Now }
+                    };
+
+                    messages.Insert(newMessage);                    
+                }
+                    
+                var formattedMessages = messages.FindAll()
+                                                .Select(m => string.Format("[{0}] {1}: {2}", m["Time"]
+                                                                   .ToLocalTime(), m["Author"], m["Text"]));
+
+                string output = string.Join(Environment.NewLine, formattedMessages);
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
---
+++ 11.Databases/11.MongoDB_HOMEWORK/ChatConsoleClient/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ChatConsoleClient
+{
+    using MongoDB.Driver;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connection = "mongodb://telerik:chat@ds035300.mongolab.com:35300/mongochat";
+            var db = GetMongoDatabase(connection);
+            
+            var chat = new Chat(db);
+            chat.Start();
+        }
+
+        private static MongoDatabase GetMongoDatabase (string connection)
+        {
+            var client = new MongoClient(connection);
+            var server = client.GetServer();
+            var db = server.GetDatabase("mongochat");
+
+            return db;
+        }
+    }
+}
