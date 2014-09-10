9785121849ccdd132d0b2cb888243f6637204ea9
theexar <easlavov@gmail.com>
2014-09-10 17:08:22 +0300

    Exam prep - added solutions to combinatorics problems

---
+++ Data Structures and Algorithms/ExamPrep/BinaryPasswords/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace BinaryPasswords
+{
+    using System;
+    using System.Linq;
+
+    class Solution
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            int asterisksCount = input.Count(character => character == '*');
+            long possiblePasswords = GetPasswordsCount(asterisksCount);
+            Console.WriteLine(possiblePasswords);
+        }
+
+        private static long GetPasswordsCount(int asterisksCount)
+        {
+            if (asterisksCount == 0)
+            {
+                return 1;
+            }
+
+            long result = 2;
+            for (int i = 0; i < asterisksCount - 1; i++)
+            {
+                result <<= 1;
+            }
+
+            return result;
+        }
+    }
+}
