452d5ec802a1bec2d99ce6852b88b072c832afe5
theexar <easlavov@gmail.com>
2014-09-11 13:11:12 +0300

    DogeCoin solution

---
+++ Data Structures and Algorithms/10.DynamicProgramming/01.KnapsackProblem/Solution.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Solution
+{
+    static void Main()
+    {
+        Product[] products =
+        {
+            new Product { Name = "beer", Weight = 3, Cost = 2 },
+            new Product { Name = "vodka", Weight = 8, Cost = 12 },
+            new Product { Name = "cheese", Weight = 4, Cost = 5 },
+            new Product { Name = "nuts", Weight = 1, Cost = 4 },
+            new Product { Name = "ham", Weight = 2, Cost = 3 },
+            new Product { Name = "whiskey", Weight = 8, Cost = 13 },
+        };
+
+        Array.Sort(products);
+        string format = "{0} W:{1}, C:{2}";
+        foreach (var item in products)
+        {
+            Console.WriteLine(string.Format(format, item.Name, item.Weight, item.Cost));
+        }
+
+        // Input:
+        // Values (stored in array v)
+        // Weights (stored in array w)
+        // Number of distinct items (n)
+        // Knapsack capacity (W)
+
+        //for j from 0 to W do
+        //  m[0, j] := 0
+        //end for 
+        //for i from 1 to n do
+        //  for j from 0 to W do
+        //    if w[i] <= j then
+        //      m[i, j] := max(m[i-1, j], m[i-1, j-w[i]] + v[i])
+        //    else
+        //      m[i, j] := m[i-1, j]
+        //    end if
+        //  end for
+        //end for
+    }
+}
+
+class Product : IComparable
+{
+    public string Name { get; set; }
+
+    public int Weight { get; set; }
+
+    public int Cost { get; set; }
+
+    public int CompareTo(object obj)
+    {
+        Product obj2 = obj as Product;
+        if (this.Weight > obj2.Weight)
+        {
+            return 1;
+        }
+        else if (this.Weight < obj2.Weight)
+        {
+            return -1;
+        }
+
+        return 0;
+    }
+}
---
+++ Data Structures and Algorithms/ExamPrep/DogeCoin/Solution.cs
@@ -0,0 +1,60 @@
+﻿namespace DogeCoin
+{
+    using System;
+
+    class Solution
+    {
+        static void Main()
+        {
+            string[] firstInputRow = Console.ReadLine().Split(' ');
+            int n = int.Parse(firstInputRow[0]);
+            int m = int.Parse(firstInputRow[1]);
+
+            int[,] coinsField = new int[n, m];
+            int[,] pathsScores = new int[n, m];
+
+            int k = int.Parse(Console.ReadLine());
+            for (int i = 0; i < k; i++)
+            {
+                string[] coords = Console.ReadLine().Split(' ');
+                int x = int.Parse(coords[0]);
+                int y = int.Parse(coords[1]);
+                coinsField[x, y] += 1;
+            }
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < m; col++)
+                {
+                    int currentCellCoins = coinsField[row, col];
+                    int leftCellCoins = 0;
+                    int upperCellCoins = 0;
+                    try
+                    {
+                        leftCellCoins = pathsScores[row, col - 1];                        
+                    }
+                    catch (Exception)
+                    {                        
+                        
+                    }
+
+                    try
+                    {
+                        upperCellCoins = pathsScores[row - 1, col];
+                    }
+                    catch (Exception)
+                    {
+                        
+                    }
+
+                    int max = Math.Max(leftCellCoins, upperCellCoins);
+                    int cellValue = currentCellCoins + max;
+                    pathsScores[row, col] = cellValue;
+                }
+            }
+
+            int result = pathsScores[n - 1, m - 1];
+            Console.WriteLine(result);
+        }
+    }
+}
