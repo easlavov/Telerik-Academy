13fae08cc319af6b86e86d90af7ab3ffccca0292
theexar <easlavov@gmail.com>
2014-09-13 11:07:51 +0300

    added trash project

---
+++ Data Structures and Algorithms/ExamPrep/Trash/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    private const int MILLION = 1000000;
+    private const int TEN_MILLION = 10000000;
+    private const int HUNDRED_MILLION = 100000000;
+    private const int BILLION = 1000000000;
+
+    static void Main()
+    {
+        var sw = new Stopwatch();
+        sw.Start();
+        for (int i = 0; i < BILLION; i++)
+        {
+            
+        }
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed);
+    }
+}
