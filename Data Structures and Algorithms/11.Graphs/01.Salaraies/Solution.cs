9deb5db23b81f2b7e221ffe15ca0b7074b156210
theexar <easlavov@gmail.com>
2014-09-14 20:41:16 +0300

    Graphs HW

---
+++ Data Structures and Algorithms/11.Graphs/01.Salaraies/Solution.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Solution
+{
+    static bool[,] employees;
+    static int count;
+    static long[] salaries;
+
+    static void Main()
+    {
+        count = int.Parse(Console.ReadLine());
+        employees = new bool[count, count];
+        salaries = new long[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            string input = Console.ReadLine();
+            for (int j = 0; j < count; j++)
+            {
+                if (input[j] == 'Y')
+                {
+                    employees[i, j] = true;
+                }
+            }
+        }
+
+        long totalSalary = 0;
+        for (int i = 0; i < count; i++)
+        {
+            totalSalary += CalcSalary(i);
+        }
+
+        Console.WriteLine(totalSalary);
+    }
+
+    static long CalcSalary(int employee)
+    {
+        if (salaries[employee] > 0)
+        {
+            return salaries[employee];
+        }
+
+        long salary = 0;
+        for (int i = 0; i < count; i++)
+        {
+            if (employees[employee, i])
+            {
+                salary += CalcSalary(i);
+            }
+        }
+
+        salary = (salary != 0 ? salary : 1);
+        salaries[employee] = salary;
+        return salary;
+    }
+}
---
+++ Data Structures and Algorithms/11.Graphs/02.TvCompany/Solution.cs
@@ -0,0 +1,89 @@
+﻿// You are given a cable TV company. The company needs to lay cable to a new neighborhood (for every house).
+// If it is constrained to bury the cable only along certain paths,
+// then there would be a graph representing which points are connected by those paths.
+// But the cost of some of the paths is more expensive because they are longer.
+// If every house is a node and every path from house to house is an edge, find a way to minimize the cost for cables.
+
+using System;
+using System.Linq;
+
+class CableNetworkKruskal
+{
+    static const int NO_PARENT = -1;
+    static int[] pred;
+
+    static void Main()
+    {
+        var edges = BuildGraph();
+        int vertexCount = edges.Max(e => Math.Max(e.Node1, e.Node2));
+        pred = Enumerable.Repeat(NO_PARENT, vertexCount + 1).ToArray();
+        FindMinSpanTree(edges);
+    }
+
+    static void FindMinSpanTree(Edge[] edges)
+    {
+        Array.Sort(edges);
+
+        Console.WriteLine("Edges in the minimum spanning tree (using Kruskal):");
+        int totalCost = 0;
+        for (int i = 0; i < edges.Length; i++)
+        {
+            int rootNode1 = FindRoot(edges[i].Node1);
+            int rootNode2 = FindRoot(edges[i].Node2);
+            if (rootNode1 != rootNode2)
+            {
+                Console.Write("({0}, {1}) ", edges[i].Node1, edges[i].Node2);
+                totalCost += edges[i].Cost;
+                pred[rootNode2] = rootNode1;
+            }
+        }
+
+        Console.WriteLine("\nThe cost of the minimum spanning tree is {0}.", totalCost);
+    }
+
+    static int FindRoot(int vertex)
+    {
+        int root = vertex;
+        while (pred[root] != NO_PARENT) root = pred[root];
+        int temp = 0;
+        while (vertex != root)
+        {
+            temp = vertex;
+            vertex = pred[vertex];
+            pred[temp] = root;
+        }
+        return root;
+    }
+
+    static Edge[] BuildGraph()
+    {
+        return new[]
+        {
+            new Edge() { Node1 = 1, Node2 = 2, Cost = 1  },
+            new Edge() { Node1 = 1, Node2 = 4, Cost = 2  },
+            new Edge() { Node1 = 2, Node2 = 3, Cost = 3  },
+            new Edge() { Node1 = 2, Node2 = 5, Cost = 13 },
+            new Edge() { Node1 = 3, Node2 = 4, Cost = 4  },
+            new Edge() { Node1 = 3, Node2 = 6, Cost = 3  },
+            new Edge() { Node1 = 4, Node2 = 6, Cost = 16 },
+            new Edge() { Node1 = 4, Node2 = 7, Cost = 14 },
+            new Edge() { Node1 = 5, Node2 = 6, Cost = 12 },
+            new Edge() { Node1 = 5, Node2 = 8, Cost = 1  },
+            new Edge() { Node1 = 5, Node2 = 9, Cost = 13 },
+            new Edge() { Node1 = 6, Node2 = 7, Cost = 1  },
+            new Edge() { Node1 = 6, Node2 = 9, Cost = 1  }
+        };
+    }
+}
+
+struct Edge : IComparable<Edge>
+{
+    public int Node1 { get; set; }
+    public int Node2 { get; set; }
+    public int Cost { get; set; }
+
+    public int CompareTo(Edge other)
+    {
+        return this.Cost.CompareTo(other.Cost);
+    }
+}
