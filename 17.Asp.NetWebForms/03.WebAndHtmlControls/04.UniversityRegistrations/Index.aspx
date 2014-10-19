920b07c5e1451cc1e3152f281287ebeadbff6a7f
theexar <easlavov@gmail.com>
2014-10-19 11:45:15 +0300

    controls task 4

--- 17.Asp.NetWebForms/03.WebAndHtmlControls/04.UniversityRegistrations/Index.aspx
+++ 17.Asp.NetWebForms/03.WebAndHtmlControls/04.UniversityRegistrations/Index.aspx
@@ -4,20 +4,54 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>Student registration</title>
     <style>
+        section {
+            border: 1px solid black;
+            width: 800px;
+            margin-bottom: 20px;
+        }
 
+        .control-group {
+            margin-bottom: 10px;
+        }
     </style>
 </head>
 <body>
     <form id="form1" runat="server">
 
         <section>
-    
+            <h3>Register student: </h3>
+            <div class="control-group">
+                <asp:Label ID="LabelFirstName" runat="server" Text="First Name: "></asp:Label>
+                <asp:TextBox ID="TextBoxFirstName" runat="server"></asp:TextBox>
+            </div>
+
+            <div class="control-group">
+                <asp:Label ID="LabelLastName" runat="server" Text="Last Name: "></asp:Label>
+                <asp:TextBox ID="TextBoxLastName" runat="server"></asp:TextBox>
+            </div>
+
+            <div class="control-group">
+                <asp:Label ID="LabelFacultyNumber" runat="server" Text="Faculty number: "></asp:Label>
+                <asp:TextBox ID="TextBoxFacultyNumber" runat="server"></asp:TextBox>
+            </div>
+
+            <asp:Label ID="LabelUniversity" runat="server" Text="University: "></asp:Label>
+            <asp:DropDownList ID="DropDownListUniversity" runat="server"></asp:DropDownList>
+
+            <asp:Label ID="LabelSpecialty" runat="server" Text="Specialty: "></asp:Label>
+            <asp:DropDownList ID="DropDownListSpecialty" runat="server"></asp:DropDownList>
+
+            <asp:Label ID="LabelCourses" runat="server" Text="Courses: "></asp:Label>
+            <asp:ListBox ID="ListBoxCourses" SelectionMode="Multiple" runat="server"></asp:ListBox>
+            <br />
+
+            <asp:Button ID="ButtonSubmit" runat="server" Text="Submit" OnClick="ButtonSubmit_Click" />
         </section>
 
         <section>
-    
+            <asp:Panel ID="PanelFeedback" runat="server"></asp:Panel>
         </section>
     </form>
 </body>
--- 17.Asp.NetWebForms/03.WebAndHtmlControls/04.UniversityRegistrations/Index.aspx.cs
+++ 17.Asp.NetWebForms/03.WebAndHtmlControls/04.UniversityRegistrations/Index.aspx.cs
@@ -1,17 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-
-namespace _04.UniversityRegistrations
+﻿namespace _04.UniversityRegistrations
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.UI;
+    using System.Web.UI.WebControls;
+
     public partial class Index : System.Web.UI.Page
     {
+        private string[] universities =
+        {
+            "Technical University",
+            "Sofia University",
+            "New Bulgarian University"
+        };
+
+        private string[] specialties =
+        {
+            "Biology",
+            "Architecture",
+            "Mathematics",
+            "Computer Science"
+        };
+
+        private string[] courses = 
+        {
+            "C# Basics",
+            "Chemistry",
+            "Javascript Advanced"
+        };
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!this.IsPostBack)
+            {
+                this.DropDownListUniversity.DataSource = universities;
+                this.DropDownListSpecialty.DataSource = specialties;
+                this.ListBoxCourses.DataSource = courses;
 
+                this.DropDownListUniversity.DataBind();
+                this.DropDownListSpecialty.DataBind();
+                this.ListBoxCourses.DataBind();
+            }
+        }
+
+        protected void ButtonSubmit_Click(object sender, EventArgs e)
+        {
+            AddNames();
+            AddNumber();
+            AddSpecialtyAndUniversity();
+            AddCourses();
+        }
+
+        private void AddNames()
+        {
+            string firstName = this.TextBoxFirstName.Text;
+            string lastName = this.TextBoxLastName.Text;
+
+            var heading = new LiteralControl();
+            heading.Text = "<h4>" + firstName + " " + lastName + "</h4>";
+            this.PanelFeedback.Controls.Add(heading);
+        }
+
+        private void AddNumber()
+        {
+            string number = this.TextBoxFacultyNumber.Text;
+            var facultyNumberField = new LiteralControl(
+                                            "<span>" + number + "</span>" + "<br /><br />");
+
+            this.PanelFeedback.Controls.Add(facultyNumberField);
+        }
+
+        private void AddSpecialtyAndUniversity()
+        {
+            string specialty = this.DropDownListSpecialty.SelectedItem.Text;
+            string university = this.DropDownListUniversity.SelectedItem.Text;
+
+            var specUniField = new LiteralControl(
+                                        "<span>" + specialty + "</span>" + " in " + university + "<br />");
+
+            this.PanelFeedback.Controls.Add(specUniField);
+        }
+
+        private void AddCourses()
+        {
+            var selectedCourses = this.ListBoxCourses.Items.OfType<ListItem>().Where(item => item.Selected);
+            var coursesField = new LiteralControl("<p>Courses:</p>");
+            foreach (var course in selectedCourses)
+            {
+                coursesField.Text += "<span>" + course + " | " + "</span>";
+            }
+
+            this.PanelFeedback.Controls.Add(coursesField);
         }
     }
 }
